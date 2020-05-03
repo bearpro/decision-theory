472a3e617eaa10f30463b8aa9831e66cc14b025a
Михаил Проказин <bearpro@outlook.com>
2020-05-03 18:45:52 +0300

    Причёсан код метода хорд.

--- DecisionTheory/Optimization.Chord.fs
+++ DecisionTheory/Optimization.Chord.fs
@@ -5,9 +5,8 @@ let methodName = "Метод Хорд"
 let rec argmin (a, b) accuracy primeFunc =
     let x = a - ((primeFunc a) / ((primeFunc a) - (primeFunc b))) * (a - b)
     let y = primeFunc x
-    if abs(y) < accuracy
-    then x
-    else
-        if y > 0.0
-        then argmin (a, x) accuracy primeFunc
-        else argmin (x, b) accuracy primeFunc
+    match abs(y) < accuracy with
+    | true  -> x
+    | false ->
+        let range = if y > 0.0 then (a, x) else (x, b)
+        argmin range accuracy primeFunc
